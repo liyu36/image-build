8f009e5d2b7725938754c5dd0bc8ca41bded509c
root <root@aliyun.liyblog.com>
2022-04-27 09:30:21 +0800

    add yearning

--- yearning/entrypoint.sh
+++ yearning/entrypoint.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env sh
 
-
-cat << EOF > /opt/conf.toml
-[Mysql]
-Db = "${MYSQL_DATABASE}"
-Host = "${MYSQL_HOST}"
-Port = "${MYSQL_PORT}"
-Password = "${MYSQL_PASSWORD}"
-User = "${MYSQL_USERNAME}"
-
-[General]
-SecretKey = "${SECRET_KEY}"
-Hours = $HOURS
-Concurrent = ${Concurrent}
-EOF
-
 /opt/yearning run 
